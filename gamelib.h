d9b5f5f15d1120dd45094ccce2fc9698478bc302
nicolasguti05 <nicolas05guti@gmail.com>
2025-03-27 17:46:10 +0100

    Create gamelib.h

---
+++ gamelib.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#define YELLOW  "\e[0;33m"
+#define RED     "\e[0;31m"
+#define BLUE    "\e[0;34m"
+#define RESET   "\e[0m"
+
+
+void imposta_gioco();
+void gioca();
+void termina_gioco();
+void crediti();
+
+
+static bool settato = false;
+
+typedef enum Classe_giocatore {principe, doppleganger} Classe_giocatore;
+typedef enum Classe_nemico {nessun_nemico,scheletro, guardia, Jaffar} Classe_nemico;
+typedef enum Tipo_stanza {corridoio, scala, sala_banchetto, magazzino, posto_guardia, prigione, armeria, moschea, torre, bagni} Tipo_stanza;
+typedef enum Tipo_trabocchetto {nessuno, tegola, lame, caduta, burrone} Tipo_trabocchetto;
+typedef enum Tipo_tesoro {nessun_tesoro, verde_veleno, blu_guarigione, rosso_aumenta_vita, spada_tagliente, scudo} Tipo_tesoro;
+
+
+
+typedef struct Giocatore {
+    char nome_giocatore [20];
+    enum Classe_giocatore classe_giocatore;
+    struct Stanza* posizione;
+    unsigned char p_vita_max;
+    unsigned char p_vita;
+    unsigned char dadi_attaco;
+    unsigned char dadi_difesa;
+    unsigned char scappatoie_usate; //campo aggiunto per gestire le scappatoie in base al giocatore
+} Giocatore;
+
+typedef struct Stanza {
+    struct Stanza* stanza_destra;
+    struct Stanza* stanza_sinistra;
+    struct Stanza* stanza_sopra;
+    struct Stanza* stanza_sotto;
+    enum Tipo_stanza tipo_stanza;
+    enum Tipo_trabocchetto tipo_trabocchetto;
+    enum Tipo_tesoro tipo_tesoro;
+    enum Classe_nemico nemico;
+} Stanza;
+
+typedef struct Nemico {
+    enum Classe_nemico nemico;
+    unsigned char vita_nemico;
+    unsigned char attacco_nemico;
+    unsigned char difesa_nemico;
+} Nemico;
